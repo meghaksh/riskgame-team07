835300f4fd1abf1590c40730257e4d32f9f39471
ZealPatel15 <zhy15798@gmail.com>
2021-02-10 23:44:28 +0530

    Initializing variables and Constructor of Player model class

--- src/main/java/org/soen6441/model/Player.java
+++ src/main/java/org/soen6441/model/Player.java
@@ -1,5 +1,40 @@
 package org.soen6441.model;
 
+
+
+/**
+ * The Player class represents the actual player participating in the game.
+ * @author Zeal
+ *
+ */
 public class Player {
-	String Playername="Alpha";
+	String d_playerName="";
+	int d_playerId;
+	String d_playerColor = "";
+
+	
+	
+	Player(String p_playerName,int p_playerId,String p_playerColor)
+	{
+		d_playerName = p_playerName;
+		d_playerId = p_playerId;
+		d_playerColor = p_playerColor;
+	}
+	
+	public String getPlayerName()
+	{
+		return d_playerName;
+	}
+	
+	public int getPlayerId()
+	{
+		return d_playerId;
+	}
+	
+	public String getPlayerColor()
+	{
+		return d_playerColor;
+	}
+	
+	
 }
