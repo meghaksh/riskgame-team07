6962a4e714ce25dda8b926e7274393e51464a25f
Sarvesh <raytersarvesh8@gmail.com>
2021-02-10 09:51:59 +0530

    Adding create and load methods to Map Class

--- src/main/java/org/soen6441/model/Map.java
+++ src/main/java/org/soen6441/model/Map.java
@@ -5,5 +5,37 @@ package org.soen6441.model;
  */
 
 public class Map {
+	
+	/**
+	 * Default constructor
+	 * 
+	 */
+	public Map()
+	{
+		
+	}
+	
+	/**
+	 * This method creates a user edited map
+	 * @param filename
+	 */
+	
+	public void CreateMap(String filename)
+	{
+		
+		
+	}
+	
+	
+	
+	
+	/**
+	 * This method loads a map file given by the user
+	 * @param filename
+	 */
+	public void LoadMap(String filename)
+	{
+		
+	}
 
 }
