78fe4501343eeb404548ecda13872f8e471f1719
AkshitaPatel <akshitapatel02@gmail.com>
2021-02-10 21:14:36 +0400

    Adding and defining data members and methods in the Order class in the model package.

--- src/main/java/org/soen6441/model/Order.java
+++ src/main/java/org/soen6441/model/Order.java
@@ -13,10 +13,10 @@ public class Order {
 	private String d_noOfArmies;
 	//private HashMap<Integer, String> d_order_list;
 	Scanner scan = new Scanner(System.in);
-	
-//	public Order(String order) {
+
+	//	public Order(String order) {
 	public Order() {
-	System.out.println("Enter command: ");
+		System.out.println("Enter command: ");
 		d_order = scan.nextLine(); 
 		//this.d_order = order;
 		run();
@@ -24,54 +24,46 @@ public class Order {
 	/**
 	 * This method is used to check and display the order the player has issued
 	 */
-	
-	public void run() {
-		
-		String[] splitted = d_order.split(" ");
-		
-		
 
-//			if(i%2!=0) {
-//				if(splitted[i].contains("-add")) {
-//					System.out.println("ADD");
-//					
-//				}
-//				else if(splitted[i].contains("-remove")) {
-//					System.out.println("REMOVE");
-//				}
-				
-				if(splitted[0].equals("-deploy")) {
-				d_countryId = splitted[1];
-				d_noOfArmies = splitted[2];
-				System.out.println("DEPLOY "+ d_noOfArmies + " armies on "+ d_countryId + " country" );
-				}
-				else {
-					System.out.println("Invalid command");
-				}
-				
-			
-		
-			
-}
+	public void run() {
+
+		String[] splitted = d_order.split(" ");
+		//			if(i%2!=0) {
+		//				if(splitted[i].contains("-add")) {
+		//					System.out.println("ADD");
+		//					
+		//				}
+		//				else if(splitted[i].contains("-remove")) {
+		//					System.out.println("REMOVE");
+		//				}
+
+		if(splitted[0].equals("-deploy")) {
+			d_countryId = splitted[1];
+			d_noOfArmies = splitted[2];
+			System.out.println("DEPLOY "+ d_noOfArmies + " armies on "+ d_countryId + " country" );
+		}
+		else {
+			System.out.println("Invalid command");
+		}
+
+	}
 	/**
 	 * This method is used to execute the orders issued by the players.
 	 */
-	
+
 	public void execute() {
-		
+
 		//CountryObject.deployArmies();
 		// but how to get country object with specific ID;
 		// we need a list of all countries object for that
-		
-	}
-	
-	public static void main(String args[]) {
-		Order or = new Order();
+
 	}
 
-	
-		
+	public static void main(String args[]) {
+		Order order = new Order();
+	}
+
 }
-	
+
 
 
