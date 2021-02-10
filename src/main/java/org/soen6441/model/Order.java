fe40cb2d423352e59607b243b29a0ea984e56984
AkshitaPatel <akshitapatel02@gmail.com>
2021-02-10 21:09:39 +0400

    Adding and defining data members and methods in the Order class

--- src/main/java/org/soen6441/model/Order.java
+++ src/main/java/org/soen6441/model/Order.java
@@ -1,4 +1,6 @@
 package org.soen6441.model;
+import java.util.Scanner;
+
 /**
  * 
  * Class for Order issues by Players
@@ -6,6 +8,67 @@ package org.soen6441.model;
  */
 
 public class Order {
+	private String d_order;
+	private String d_countryId;
+	private String d_noOfArmies;
+	//private HashMap<Integer, String> d_order_list;
+	Scanner scan = new Scanner(System.in);
+	
+//	public Order(String order) {
+	public Order() {
+	System.out.println("Enter command: ");
+		d_order = scan.nextLine(); 
+		//this.d_order = order;
+		run();
+	}
+	/**
+	 * This method is used to check and display the order the player has issued
+	 */
+	
+	public void run() {
+		
+		String[] splitted = d_order.split(" ");
+		
+		
+
+//			if(i%2!=0) {
+//				if(splitted[i].contains("-add")) {
+//					System.out.println("ADD");
+//					
+//				}
+//				else if(splitted[i].contains("-remove")) {
+//					System.out.println("REMOVE");
+//				}
+				
+				if(splitted[0].equals("-deploy")) {
+				d_countryId = splitted[1];
+				d_noOfArmies = splitted[2];
+				System.out.println("DEPLOY "+ d_noOfArmies + " armies on "+ d_countryId + " country" );
+				}
+				else {
+					System.out.println("Invalid command");
+				}
+				
+			
+		
+			
+}
+	/**
+	 * This method is used to execute the orders issued by the players.
+	 */
+	
+	public void execute() {
+		
+		//CountryObject.deployArmies();
+		// but how to get country object with specific ID;
+		// we need a list of all countries object for that
+		
+	}
+	
+	public static void main(String args[]) {
+		Order or = new Order();
+	}
+
 	
 		
 }
