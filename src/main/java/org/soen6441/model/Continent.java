2daf4ceda3d8985afe4a9d7b4f2e8fcaf7896978
AkshitaPatel <akshitapatel02@gmail.com>
2021-02-11 13:23:08 +0400

    Updating data members of Continent.java in Model according to the coding conventions.

--- src/main/java/org/soen6441/model/Continent.java
+++ src/main/java/org/soen6441/model/Continent.java
@@ -7,19 +7,19 @@ package org.soen6441.model;
 
 public class Continent {
 	
-	static int ID=0;
-	String Name;
-	int Continent_Control_Value;
+	static int d_ID=0;
+	String d_Name;
+	int d_ContinentControlValue;
 	
 	
 	/*
 	 * default constructor adding id, name and control value of continent to object
 	 */
-	public Continent(String Name, int Continent_Control_Value)
+	public Continent(String p_Name, int p_ContinentControlValue)
 	{
-		this.ID=ID++;
-		this.Name=Name;
-		this.Continent_Control_Value=Continent_Control_Value;
+		this.d_ID=d_ID++;
+		this.d_Name=p_Name;
+		this.d_ContinentControlValue=p_ContinentControlValue;
 	}
 
 }
