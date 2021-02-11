73a9b0a978e38ffcb58ad94b6913c9ef82571850
rokaliraj <rokalirajkumar@gmail.com>
2021-02-11 20:20:44 +0530

    Adding add and remove player methods

--- src/main/java/org/soen6441/controller/GameEngine.java
+++ src/main/java/org/soen6441/controller/GameEngine.java
@@ -1,7 +1,59 @@
 package org.soen6441.controller;
+import java.util.Scanner;
 
+import org.soen6441.model.*;
 
 
 public class GameEngine {
+	GameModel d_GameModel_Object;
+	Scanner d_s = new Scanner(System.in);
+	private String d_playerName;
+	
+	
+
+	
+	/**
+	 * This Method Will take input from player and add into the list
+	 * 
+	 * 
+	 */
+	private void addPlayer()
+	{
+	
+		
+	}
+	/**
+	 * This Method Will take input from player and remove from the list
+	 * army to each country for a player
+	 * 
+	 */
+	private void removePlayer()
+	{
+	
+		
+	}
+	
+	/**
+	 * This Method initializes the game by assigning armies and countries to players
+	 * army to each country for a player
+	 * 
+	 */
+	private void InitializeGame() 
+	{
+		
+		//d_GameModel_Object.startUpPhase();
+
+
+	}
+	private void ShowMap() 
+	{
+
+
+	}
+	private void LoadMap() 
+	{
+
+
+	}
 	
 }
