03ce0e3ca23644145c40aa117a854dd4a0088518
rokaliraj <rokalirajkumar@gmail.com>
2021-02-11 19:25:26 +0530

    Adding PLayer getting current player set and get methods

--- src/main/java/org/soen6441/model/GameModel.java
+++ src/main/java/org/soen6441/model/GameModel.java
@@ -1,7 +1,6 @@
 package org.soen6441.model;
 
-
-
+import java.util.ArrayList;
 
 public class GameModel 
 {	/**
@@ -13,6 +12,8 @@ public class GameModel
 	}
 	private Phases d_gamePhase;
 	private Map d_map;
+	private Player d_PlayerID;
+	private ArrayList<Player> d_PlayerList = new ArrayList<Player>();
 	
 	/**
 	 * This is a constructor of GameModel class which will initialize the Map and set the gamePhase 
@@ -38,6 +39,23 @@ public class GameModel
 	 */
 	public void setGamePhase(Phases d_gamePhase) {
 		this.d_gamePhase = d_gamePhase;
+	}
+	/**
+	 * This method returns the  player id.
+	 * @return d_PlayerID of current  player.
+	 */
+	public Player getPlayerId() 
+	{
+		return d_PlayerID;
+	}
+	/**
+	 * This method returns the  player id.
+	 * @return d_PlayerID of current  player.
+	 */
+	public void setPlayerId(Player d_PlayerID) 
+	{
+		
+		this.d_PlayerID = d_PlayerID;
 	}
 
 
