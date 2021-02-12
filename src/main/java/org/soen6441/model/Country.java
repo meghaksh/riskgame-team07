6c3a195a58952078737bb0b7f506be6bc6632b0a
AkshitaPatel <akshitapatel02@gmail.com>
2021-02-12 23:21:25 +0400

    Adding methods getContinentID(), getCountyName(), getCountryID()

--- src/main/java/org/soen6441/model/Country.java
+++ src/main/java/org/soen6441/model/Country.java
@@ -12,14 +12,15 @@ public class Country {
 	String d_Name;
 	int d_ContinentID;
 	ArrayList<Integer> d_neighbors;
+	int d_NoOfArmies;
 
 
 
 
 	/**
-	 * default constructor adding the id , name and continent id parameters to object 
+	 * default constructor adding the id , name and continent id parameters to object
+	 * @param CountryId, Country Name, Continent Id to which it belongs 
 	 */
-
 	public Country(int p_ID, String p_Name, int p_ContinentID)
 	{
 
@@ -30,7 +31,8 @@ public class Country {
 	}
 
 	/**
-	 * Setting the neighbors/borders for each territory/country
+	 * Method to set the borders of neighboring Countries
+	 * @param borders
 	 */
 	public  void setBorder(int borders)
 	{
@@ -39,11 +41,34 @@ public class Country {
 
 
 	/**
-	 * Getting the neighbors/borders for each territory/country
+	 * Method to get the list of neighboring Countries
+	 * @return
 	 */
 	public  ArrayList<Integer> getBorder()
 	{
 		return this.d_neighbors;
+	}
+	
+	/**
+	 * Method to get Continent ID
+	 * @return Continent ID
+	 */
+	public int getContinentId() {
+		return d_ContinentID;	
+	}
+	/**
+	 * Method to get Country Name
+	 * @return Country Name
+	 */
+	public String getCountryName() {
+		return d_Name;
+	}
+	/**
+	 * Method to get Country ID
+	 * @return COuntry ID
+	 */
+	public int getCountryID() {
+		return d_ID;
 	}
 
 }
