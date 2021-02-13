34d7943c9658b9034e6bcac6c813c23f8a54e6bf
ZealPatel15 <zhy15798@gmail.com>
2021-02-13 22:31:56 +0530

    Adding player_issue_order in Player Controller

--- src/main/java/org/soen6441/controller/PlayerController.java
+++ src/main/java/org/soen6441/controller/PlayerController.java
@@ -16,13 +16,30 @@ public class PlayerController {
 		d_scan =new Scanner(System.in);
 		d_players = p_players;
 	}
-	/*Iterator it = d_players.iterator();
-	
-	while(it.hasNext())
+	public void player_issue_order()
 	{
-		String order = d_scan.nextLine();
-		Player l_tempPlayer = (Player)it.next();
-		l_tempPlayer.issue_order();
+		while(!d_players.isEmpty())
+		{
+			Iterator it = d_players.iterator();
+			int l_flag =0;
+			Player l_removePlayer = new Player();
+			while(it.hasNext())
+			{
+				l_flag = 0;
+				String l_stringOrder = d_scan.nextLine();
+				Player l_tempPlayer = (Player)it.next();
+				l_tempPlayer.setOrder(l_stringOrder);
+				l_tempPlayer.issue_order();
+				String l_result = l_tempPlayer.getResult();
+
+				if(l_result.contains("country"))
+				{
+					System.out.println(l_result);
+				}
+			}
+			
+		}
+		
 	}
-	*/
+	
 }
