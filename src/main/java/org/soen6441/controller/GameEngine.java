7ea370f5ef61dfeef4f888ba19c4b6504e289362
rokaliraj <rokalirajkumar@gmail.com>
2021-02-17 23:43:23 +0530

    Adding GamePlayer Method

--- src/main/java/org/soen6441/controller/GameEngine.java
+++ src/main/java/org/soen6441/controller/GameEngine.java
@@ -1,37 +1,86 @@
 package org.soen6441.controller;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import org.soen6441.model.*;
 
 
 public class GameEngine {
-	GameModel d_GameModel_Object;
+	
+	
 	Scanner d_s = new Scanner(System.in);
-	private String d_playerName;
+	private String d_PlayerName;
+	private Map d_Map;
+	private ArrayList<Player> d_PlayerList;
+	GameModel d_GameModel_Object = new GameModel(d_Map);
 	
 	
+	public GameEngine(Map p_Map ){
+		d_Map = p_Map;
+	}
+	public List<String> showall()
+	{
+		List<String> Names = new ArrayList<>();
 
+		d_PlayerList=d_GameModel_Object.getAllPlayers();
+		for(Player player:d_PlayerList)
+		{
+			Names.add(player.getPlayerName()+"->"+player.getCountriesSize());
+		}
+		
+		return Names; 
+		
+	}
 	
 	/**
 	 * This Method Will take input from player and add into the list
 	 * 
 	 * 
 	 */
-	private void addPlayer()
+	public String GamePlayer(String p_Command,String p_Str) throws Exception
 	{
+		String[] l_commandArray = p_Str.split(" ");
+		int l_Counter = 1;
+		int l_AddCounter = 0;
+		int l_RemoveCounter = 0;
+		String l_ReturnString = "";
+		while(l_Counter<l_commandArray.length) {
+			if(l_commandArray[l_Counter].equals("-add")) 
+			{
+				d_GameModel_Object.addPlayer(l_commandArray[l_Counter+1]);
+				l_Counter+=2;
+				l_AddCounter+=1;
+				
+			}
+			else if(l_commandArray[l_Counter].equals("-remove")) 
+			{
+				d_GameModel_Object.removePlayer(l_commandArray[l_Counter+1]);
+				l_Counter+=2;
+				l_RemoveCounter+=1;
+			}
+			else 
+			{
+				
+				break;
+			}
+			}
+		if(l_AddCounter>0) {
+			l_ReturnString += "Number of Players Added : " + l_AddCounter + "\n";
+		}
+		if(l_RemoveCounter>0) {
+			l_ReturnString += "Number of Players Removed : " + l_RemoveCounter + "\n";
+		}
+		
+		return l_ReturnString;
+	
+		
 	
 		
 	}
-	/**
-	 * This Method Will take input from player and remove from the list
-	 * army to each country for a player
-	 * 
-	 */
-	private void removePlayer()
-	{
 	
-		
-	}
+
+	
 	
 	/**
 	 * This Method initializes the game by assigning armies and countries to players
