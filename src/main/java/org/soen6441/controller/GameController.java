866ee56f1d093dcfa2ba9d070a6a879b89b1d940
Meghaksh Brahmbhatt <meghaksh4footprints@gmail.com>
2021-02-19 22:12:12 +0530

    Added player logic in GameController and GameModelNew

--- src/main/java/org/soen6441/controller/GameController.java
+++ src/main/java/org/soen6441/controller/GameController.java
@@ -3,21 +3,95 @@ package org.soen6441.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.List;
+
 import org.soen6441.model.Continent;
 import org.soen6441.model.Country;
 import org.soen6441.model.GameModelNew;
+import org.soen6441.model.Player;
 import org.soen6441.view.CommandPrompt;
 
 public class GameController {
-	private GameModelNew d_GameModel;
+	private GameModelNew d_GameModelNew;
 	private CommandPrompt d_CpView;
 	private MapController d_MapController;
+	private GameEngine d_GameEngine;
+	private ArrayList<Player> d_PlayerList;
 	public GameController(CommandPrompt p_CpView, GameModelNew p_GameModel) {
-		d_GameModel = p_GameModel;
+		d_GameModelNew = p_GameModel;
 		d_CpView = p_CpView;
-		d_MapController = new MapController(this.d_GameModel.getMap());
+		d_MapController = new MapController(this.d_GameModelNew.getMap());
 		d_CpView.commandSendButtonListener(new CommandListener());
 	}
+	
+	public String GamePlayer(String p_Command,String p_Str) throws Exception
+	{
+		String[] l_commandArray = p_Str.split(" ");
+		int l_Counter = 1;
+		int l_AddCounter = 0;
+		int l_RemoveCounter = 0;
+		String l_ReturnString = "";
+		while(l_Counter<l_commandArray.length) {
+			if(l_commandArray[l_Counter].equals("-add")) 
+			{
+				d_GameModelNew.addPlayer(l_commandArray[l_Counter+1]);
+				l_Counter+=2;
+				l_AddCounter+=1;
+				
+			}
+			else if(l_commandArray[l_Counter].equals("-remove")) 
+			{
+				d_GameModelNew.removePlayer(l_commandArray[l_Counter+1]);
+				l_Counter+=2;
+				l_RemoveCounter+=1;
+			}
+			else 
+			{
+				
+				break;
+			}
+			}
+		if(l_AddCounter>0) {
+			l_ReturnString += "Number of Players Added : " + l_AddCounter + "\n";
+		}
+		if(l_RemoveCounter>0) {
+			l_ReturnString += "Number of Players Removed : " + l_RemoveCounter + "\n";
+		}
+		
+		return l_ReturnString;
+	}
+	
+	
+	public void AssignCountries()
+	{
+		d_GameModelNew.startUpPhase();
+	}
+	
+	public List<String> showall()
+	{
+		List<String> Names = new ArrayList<>();
+
+		d_PlayerList=d_GameModelNew.getAllPlayers();
+		for(Player player:d_PlayerList)
+		{
+			Names.add(player.getPlayerName()+"->"+player.getCountriesSize());
+		}
+		
+		return Names; 
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	class CommandListener implements ActionListener{
 		private boolean d_mapDone = false;
@@ -55,7 +129,7 @@ public class GameController {
 							System.out.println("Call gameplay wala showmap");
 						}else {
 							System.out.println("Normal showmap");
-							ArrayList<Continent> l_continentList = d_GameModel.getMap().getContinentList();
+							ArrayList<Continent> l_continentList = d_GameModelNew.getMap().getContinentList();
 							if(l_continentList.size()>0) {
 								for(Continent l_continent:l_continentList) {
 									d_CpView.setCommandAcknowledgement(l_continent.getContinentName() + "--->");
@@ -92,11 +166,19 @@ public class GameController {
 						}catch(Exception exp) {
 							d_CpView.setCommandAcknowledgement(exp.getMessage());
 						}
-						
 						break;
 					case "gameplayer":
+						try {
+							String l_AckMsg1 = GamePlayer("GamePlayer", str);
+							d_CpView.setCommandAcknowledgement(l_AckMsg1 + "\n");
+						}catch(Exception p_Exception) {
+							d_CpView.setCommandAcknowledgement(p_Exception.getMessage());
+							d_CpView.setCommandAcknowledgement("\n");
+						}
 						break;
 					case "assigncountries":
+						AssignCountries();
+						showall();
 						break;
 					case "deploy":
 						break;
--- src/main/java/org/soen6441/model/GameModelNew.java
+++ src/main/java/org/soen6441/model/GameModelNew.java
@@ -1,12 +1,216 @@
 package org.soen6441.model;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+
 public class GameModelNew {
 	private Map d_map;
+	private Player d_PlayerID;
+	private ArrayList<Player> d_PlayerList;
+	private int d_PlayerCount;
+	private Queue<Player> d_PlayerQueue= new LinkedList<Player>();
+	
 	public GameModelNew() {
 		d_map = new Map();
+		d_PlayerList=new ArrayList<Player>();
 	}
 	
 	public Map getMap() {
 		return this.d_map;
 	}
+	
+	
+	
+	public Player getPlayerId1() 
+	{
+		return d_PlayerID;
+	}
+	/**
+	 * This method returns the  player id.
+	 * @return d_PlayerID of current  player.
+	 */
+	public void setPlayerId(Player d_PlayerID) 
+	{
+		
+		this.d_PlayerID = d_PlayerID;
+	}
+	/**
+	 * this method will get all the players from the ArrayList
+	 * 
+	 * @return d_PlayerList, ArrayList of all the available players from player class
+	 */
+	public ArrayList<Player> getAllPlayers() {
+		return d_PlayerList;
+	}
+	/**
+	 * this method sets the  player count
+	 *
+	 * @return the GameModel
+	 */
+	public void setplayerCount() {
+		this.d_PlayerCount=this.d_PlayerList.size();
+			
+	}
+	
+	public Player getPlayerId() {
+		return this.d_PlayerID;
+	}
+	
+	
+	/**
+	 * This method gets selected map.
+	 *
+	 * @return the selected map
+	 */
+	public Map getSelectedMap() {
+		
+		return d_map;
+
+	}
+	/**
+	 * Adds the player.
+	 *
+	 * @param playerName the player name
+	 * @return Player name  that has added
+	 */
+	public void addPlayer(String p_PlayerName)throws Exception {
+//		if ((d_PlayerList.size() == d_PlayerCount && d_PlayerCount != 0) || d_PlayerList.size() == MAX_PLAYERS) 
+//		{
+//			return "Reached Max Number of Players can be added to the game";
+//		}
+		if (existDuplicatePlayer(p_PlayerName)) {
+			throw new Exception("Please enter a differnt Player name as this name already exists");
+
+		} else {
+			Player l_Player_Object = new Player(p_PlayerName);
+			d_PlayerList.add(l_Player_Object);
+			//return "Player " + p_PlayerName + " added to the game";
+		}
+
+	}
+
+	/**
+	 * This method checks for duplicate players
+	 *
+	 * @param playerName Name of the player
+	 * @return true, if there is any duplicates
+	 */
+	public boolean existDuplicatePlayer(String p_PlayerName) {
+		for (Player Player : d_PlayerList )
+			if (Player .getPlayerName().equalsIgnoreCase(p_PlayerName))
+				return true;
+
+		return false;
+
+	}
+	/**
+	 * This Method removes players 
+	 *
+	 * @param playerName the player name
+	 * @return the string
+	 */
+	public void removePlayer(String p_PlayerName) throws Exception
+	{
+		Player l_CurrentPlayer;
+		boolean l_PlayerFound = false;
+		for (Player player:d_PlayerList) {
+			l_CurrentPlayer = player;
+			if (l_CurrentPlayer.getPlayerName().equalsIgnoreCase(p_PlayerName)) {
+				l_PlayerFound = true;
+				d_PlayerList.remove(d_PlayerList.indexOf(player));
+				//return ("Player " + p_PlayerName + " This Player removed from the game");
+
+			}
+		}
+		if (l_PlayerFound == false) {
+			throw new Exception("\"This Player not found");
+	
+		}
+		//return " ";
+
+	}
+	/**
+	 * This method sets the player queue
+	 *
+	 * @param d_PlayerQueue the Player queue
+	 *
+	 */
+	public  void setplayerQueue(Queue<Player> d_PlayerQueue) {
+		this.d_PlayerQueue=d_PlayerQueue;
+				
+	}
+	/**
+	 * This method sets army count to all players.
+	 *
+	 * @param count the assigned army count 
+	 */
+	public void setPlayerArmies(int p_count) {
+		for (Player l_Player : getAllPlayers()) {
+			l_Player.setPlayerArmies(p_count);
+		}
+	}
+	/**
+	 * This method Assign all the countries randomly to the players
+	 *
+	 * 
+	 */
+	
+	public void startUpPhase() 
+	{
+
+		
+		d_PlayerQueue.addAll(getAllPlayers());
+		List<Country> l_CountryList = new ArrayList<>();
+
+		l_CountryList  = getSelectedMap().getCountryList();		
+		while (l_CountryList.size() > 0) 
+		{	//System.out.println("we are in main");
+			Random l_Random = new Random();
+			int l_index = l_Random.nextInt(l_CountryList.size());
+			setPlayerId(d_PlayerQueue.remove());
+			getPlayerId1().addCountry(l_CountryList.get(l_index));
+			//getSelectedMap().searchCountry(l_CountryList.get(l_index)).setOwnedBy(d_PlayerID);
+			//getSelectedMap().getCountryList().setOwnedBy(d_PlayerID);
+			d_PlayerQueue.add(d_PlayerID);
+			l_CountryList.remove(l_index);
+		}
+		
+		//AssignReinforcementArmies();
+		
+
+
+
+	}
+	
+	/**
+	 * This Method will assign armies to the players.
+	 */
+	public void AssignReinforcementArmies()
+	{
+//		int l_ArmyCount = ((d_PlayerID.getCountriesOwned().size())/3);
+//		//for(Continent l_Continent:getSelectedMap().getOwnedContinents(d_PlayerID.getPlayerName())) 
+//		{
+//			//l_ArmyCount += l_Continent.getControlValue();
+//		}
+//		l_ArmyCount += d_PlayerID.getPlayerArmies();
+//		l_ArmyCount= Math.max(l_ArmyCount, 3);
+//		d_PlayerID.setPlayerArmies(l_ArmyCount);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
