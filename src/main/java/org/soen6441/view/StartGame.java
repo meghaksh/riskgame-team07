1e8b5e309d80455bc76369e9fb64da0734490e0e
Meghaksh Brahmbhatt <meghaksh4footprints@gmail.com>
2021-02-19 21:04:11 +0530

    Adding the Main Game Controller and Model

--- src/main/java/org/soen6441/view/StartGame.java
+++ src/main/java/org/soen6441/view/StartGame.java
@@ -1,13 +1,15 @@
 package org.soen6441.view;
 
-import org.soen6441.controller.MapController;
-import org.soen6441.model.Map;
+import org.soen6441.controller.GameController;
+import org.soen6441.model.GameModelNew;
 
 public class StartGame {
 	public static void main(String[] args) {
-		Map d_mapModel = new Map();
-		CommandPrompt d_cpView =  new CommandPrompt();
-		MapController d_mapController = new MapController(d_cpView, d_mapModel);
+		//Map d_mapModel = new Map();
+		GameModelNew d_GameModel = new GameModelNew();
+		CommandPrompt d_CpView =  new CommandPrompt();
+		GameController d_GameController = new GameController(d_CpView, d_GameModel);
+		//MapController d_MapController = new MapController(d_cpView, d_mapModel);
 		
 	}
 }
