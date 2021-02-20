869b200a2f0e64276a5228ed3ec00c3cf2f2d42c
ZealPatel15 <zhy15798@gmail.com>
2021-02-20 22:46:20 +0530

    Taking input in PlayerController

--- src/main/java/org/soen6441/controller/PlayerController.java
+++ src/main/java/org/soen6441/controller/PlayerController.java
@@ -2,10 +2,12 @@ package org.soen6441.controller;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 import org.soen6441.model.Order;
 import org.soen6441.model.Player;
+import org.soen6441.view.CommandPrompt;
 /**
  * The Player Controller class controls the activities of all the players at once.
  * @author Zeal
@@ -13,18 +15,32 @@ import org.soen6441.model.Player;
  */
 public class PlayerController {
 
-	Scanner d_scan;
+	
 	ArrayList <Player> d_players;
-	String d_orderString="";
+	String d_orderString=null;
+	String d_orderAcknowledgment="";
+	private CommandPrompt d_CpView;
 	
 	PlayerController(ArrayList p_players)
 	{
-		d_scan =new Scanner(System.in);
 		d_players = p_players;
+	}
+	PlayerController(ArrayList p_players,CommandPrompt p_CpView)
+	{
+		d_players = p_players;
+		d_CpView=p_CpView;
 	}
 	public void setOrderFromView(String p_orderString)
 	{
 		d_orderString = p_orderString;
+	}
+	public String getOrderAcknowledgment()
+	{
+		return d_orderAcknowledgment;
+	}
+	public void setOrderString(String p_OrderString)
+	{
+		this.d_orderString=p_OrderString;
 	}
 	/**
 	 * The player_issue_order method asks each player to issue an order in a round robin fashion.
@@ -33,25 +49,45 @@ public class PlayerController {
 	 */
 	public void player_issue_order()
 	{
+		System.out.println("in player issue order");
 		ArrayList <Player> l_players = d_players;
+		for(Player p:d_players)
+		{
+			System.out.println(p.getPlayerName());
+		}
 		
 		while(!l_players.isEmpty())
 		{
-			Iterator it = l_players.iterator();
+System.out.println("inside while loop");
+			
 			int l_flag =0;
 			Player l_removePlayer = new Player();
-			while(it.hasNext())
+			for(Player l_tempPlayer : l_players)
 			{
+			
 				l_flag = 0;
-				String l_stringOrder = d_scan.nextLine();
-				//String l_stringOrder = getOrderString();
-				Player l_tempPlayer = (Player)it.next();
+				System.out.println("inside nested while loop");
+				d_orderAcknowledgment = l_tempPlayer.getPlayerName()+" Enter deploy order";
+				d_CpView.setCommandAcknowledgement(d_orderAcknowledgment);
+				
+				String l_stringOrder = JOptionPane.showInputDialog("Please Enter Your Deploy Order");
+
+				
+				System.out.println("after wait for command " + l_stringOrder);
+
+
+				
+				System.out.println(l_stringOrder);
+
 				l_tempPlayer.setOrder(l_stringOrder);
 				l_tempPlayer.issue_order();
 				String l_result = l_tempPlayer.getResult();
 				if(l_result.contains("armies"))
 				{
-					l_flag=1;System.out.println(l_result+"/n Please enter the next order accordingly");
+					l_flag=1;
+					d_orderAcknowledgment = l_result+"/n Please enter the next order accordingly";
+					System.out.println(d_orderAcknowledgment);
+					
 					int l_armies = l_tempPlayer.getPlayerArmies();
 					if(l_armies==0)
 					{
@@ -61,7 +97,9 @@ public class PlayerController {
 				if(l_result.contains("country"))
 				{
 					System.out.println(l_result);
+					d_orderAcknowledgment = l_result;
 				}
+			
 			}
 			if(l_flag==1)
 			{
