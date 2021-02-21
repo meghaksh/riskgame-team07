e285e32ecc5aefdb1530ae233cf3b3832a801ec3
Meghaksh Brahmbhatt <meghaksh4footprints@gmail.com>
2021-02-21 13:55:47 +0530

    variable name change in command prompt and start game

--- src/main/java/org/soen6441/view/CommandPrompt.java
+++ src/main/java/org/soen6441/view/CommandPrompt.java
@@ -1,135 +1,91 @@
 package org.soen6441.view;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout;			import java.awt.Color;
+import java.awt.GridLayout;				import java.awt.event.ActionListener;
+import javax.swing.JButton;				import javax.swing.JFrame;
+import javax.swing.JPanel;				import javax.swing.JScrollPane;
+import javax.swing.JTextArea;			import javax.swing.JTextField;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-
-import org.soen6441.controller.MapController;
-import org.soen6441.model.Map;
-
+/**
+ * This class is the View part of the MVC architecture. The window interacts with the user and show feedback. 
+ *
+ */
 public class CommandPrompt {
-	private MapController d_mapCtrl;
-	private Map d_map;
-	JFrame l_CommandPromptWindow = new JFrame("Command Prompt");
-	JPanel commandPromptPanel = new JPanel(new GridLayout(1,2));
-	JTextField commandInput = new JTextField(100);
-	JTextArea l_CommandAcknowledgeArea = new JTextArea(20,1);
-	JButton commandSendButton = new JButton("Execute");
-	JScrollPane l_AckAreaScrollPane = new JScrollPane(l_CommandAcknowledgeArea,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-	
+	private JFrame d_CommandPromptWindow;
+	private JPanel d_CommandPromptPanel;
+	private JTextField d_CommandInput;
+	private JTextArea d_CommandAcknowledgeArea;
+	private JButton d_CommandSendButton;
+	private JScrollPane d_AckAreaScrollPane;
+
+	/**
+	 * This is a default constructor which initialize the window and its components to display. 
+	 */
 	public CommandPrompt(){
-		//d_map = new Map();
-		//d_mapCtrl = new MapController(d_map);
+		d_CommandPromptWindow = new JFrame("Command Prompt");
+		d_CommandPromptPanel = new JPanel(new GridLayout(1,2));
+		d_CommandInput = new JTextField(100);
+		d_CommandAcknowledgeArea = new JTextArea(20,1);
+		d_CommandSendButton = new JButton("Execute");
+		d_AckAreaScrollPane = new JScrollPane(d_CommandAcknowledgeArea,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		drawWindow();
 	}
-	
+
+	/**
+	 * This method is to access text from the private input text field.
+	 * 
+	 * @return a string which was entered by user in the text field.
+	 */
 	public String getCommandInput() {
-		return this.commandInput.getText();
+		return this.d_CommandInput.getText();
 	}
-	public void setCommandAcknowledgement(String p_str) {
-		this.l_CommandAcknowledgeArea.append(p_str);
+
+	/**
+	 * This method is to append acknowledgement messages on the window.
+	 * 
+	 * @param p_str string which is passed to the append method of the text area which displays information.
+	 */
+	public void setCommandAcknowledgement(String p_Str) {
+		this.d_CommandAcknowledgeArea.append(p_Str);
 	}
-	public void setCommandInput(String p_str) {
-		this.commandInput.setText(p_str);
+
+	/**
+	 * This method sets the input to the text field where user is giving commands
+	 * 
+	 * @param p_Str string to set to the command input text field. 
+	 */
+	public void setCommandInput(String p_Str) {
+		this.d_CommandInput.setText(p_Str);
 	}
+
+	/**
+	 * This method draws the window displayed to the user. 
+	 * It consist of java swing functionality for different components. 
+	 */
 	public void drawWindow() {
-		//boolean l_MapDone = false;
-		
-		l_CommandAcknowledgeArea.setEditable(false);
-		l_CommandAcknowledgeArea.setBackground(Color.black);
-		l_CommandAcknowledgeArea.setForeground(Color.cyan);
-		l_CommandAcknowledgeArea.append("Welcome to RISK DOMINATION !!\n\n");
-		l_CommandAcknowledgeArea.append("Below are the list of commands you can use : \n");
-		l_CommandAcknowledgeArea.append("editcontinent -add continentID continentvalue -remove continentID \n"
+		d_CommandAcknowledgeArea.setEditable(false);
+		d_CommandAcknowledgeArea.setBackground(Color.black);
+		d_CommandAcknowledgeArea.setForeground(Color.cyan);
+		d_CommandAcknowledgeArea.append("Welcome to RISK DOMINATION !!\n\n");
+		d_CommandAcknowledgeArea.append("Below are the list of commands you can use : \n");
+		d_CommandAcknowledgeArea.append("editcontinent -add continentID continentvalue -remove continentID \n"
 				+ "editcountry -add countryID continentID -remove countryID \n"
 				+ "editneighbor -add countryID neighborcountryID -remove countryID neighborcountryID \n");
-		
-		commandPromptPanel.add(commandInput);
-		commandPromptPanel.add(commandSendButton);
-		l_CommandPromptWindow.add(l_AckAreaScrollPane, BorderLayout.PAGE_START);
-		l_CommandPromptWindow.add(commandPromptPanel, BorderLayout.SOUTH);
-		l_CommandPromptWindow.setSize(400,400);
-		l_CommandPromptWindow.setVisible(true);
-		l_CommandPromptWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
-		
-		
-		
-		/*
-		commandSendButton.addActionListener(e -> {
-			String str = commandInput.getText().trim();
-			switch(str.split(" ")[0]) {
-				case "editcontinent" : 
-					try {
-						String l_AckMsg = d_mapCtrl.EditMap("editcontinent", str);
-						l_CommandAcknowledgeArea.append(l_AckMsg + "\n");
-					}catch(Exception p_Exception) {
-						l_CommandAcknowledgeArea.append(p_Exception.getMessage());
-						l_CommandAcknowledgeArea.append("\n");
-					}
-					break;
-				case "editcountry" :
-					try {
-						String l_AckMsg = d_mapCtrl.EditMap("editcountry", str);
-						l_CommandAcknowledgeArea.append(l_AckMsg + "\n");
-					}catch(Exception p_Exception) {
-						l_CommandAcknowledgeArea.append(p_Exception.getMessage());
-						l_CommandAcknowledgeArea.append("\n");
-					}
-					
-					break;
-				case "editneighbour" :
-					break;
-				case "showmap":
-					if(l_MapDone) {
-						System.out.println("Call gameplay wala showmap");
-					}else {
-						System.out.println("Normal showmap");
-						ArrayList<Continent> l_continentList = d_map.getContinentList();
-						if(l_continentList.size()>0) {
-							for(Continent l_continent:l_continentList) {
-								l_CommandAcknowledgeArea.append(l_continent.getContinentName() + "--->");
-								ArrayList<Country> l_countryList = l_continent.getCountryList();
-								for(Country l_country:l_countryList) {
-									l_CommandAcknowledgeArea.append(l_country.getCountryName() + ",");
-								}
-								l_CommandAcknowledgeArea.append("\n");
-							}
-						}
-					}
-					break;
-				case "savemap":
-					break;
-				case "editmap":
-					break;
-				case "validatemap":
-					break;
-				case "loadmap":
-					break;
-				case "gameplayer":
-					
-					break;
-				case "assigncountries":
-					break;
-				case "deploy":
-					break;
-				default:
-					l_CommandAcknowledgeArea.append("Invalid Command. Please try again.\n");
-					break;
-			}
-			commandInput.setText("");
-		});*/
+		d_CommandPromptPanel.add(d_CommandInput);
+		d_CommandPromptPanel.add(d_CommandSendButton);
+		d_CommandPromptWindow.add(d_AckAreaScrollPane, BorderLayout.PAGE_START);
+		d_CommandPromptWindow.add(d_CommandPromptPanel, BorderLayout.SOUTH);
+		d_CommandPromptWindow.setSize(400,400);
+		d_CommandPromptWindow.setVisible(true);
+		d_CommandPromptWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	public void commandSendButtonListener(ActionListener listenForCommandButton) {
-		commandSendButton.addActionListener(listenForCommandButton);
+
+	/**
+	 * This method registers the action listener to the execute button
+	 * 
+	 * @param p_ListenForCommandButton ActionListener to be registered with the button
+	 */
+	public void commandSendButtonListener(ActionListener p_ListenForCommandButton) {
+		d_CommandSendButton.addActionListener(p_ListenForCommandButton);
 	}
 }
--- src/main/java/org/soen6441/view/StartGame.java
+++ src/main/java/org/soen6441/view/StartGame.java
@@ -1,20 +1,23 @@
 package org.soen6441.view;
 
-import org.soen6441.controller.GameController;
-import org.soen6441.model.GameModelNew;
+import org.soen6441.controller.GameController;	import org.soen6441.model.GameModelNew;
+
 /**
- * Driver class for the game.  
+ * Driver class for the game. It consist a single method main which is an entry point to the game.
+ *  
  */
 public class StartGame {
+
 	/**
-	 * This main method is an entry point for the game. It initialize the GameModel, GameView (aka CommandPrompt) and GameController. Passes view and model in controller.
+	 * This main method is an entry point for the game. 
+	 * It initialize the GameModel, GameView (i.e. CommandPrompt) and GameController. 
+	 * Passes view and model in controller.
 	 * 
-	 * @param args default parameter for the main method which is not used anywhere in the game. 
+	 * @param p_Args default parameter for the main method which is not used anywhere in the game. 
 	 */
-	public static void main(String[] args) {
-		GameModelNew d_GameModel = new GameModelNew();
-		CommandPrompt d_CpView =  new CommandPrompt();
-		GameController d_GameController = new GameController(d_CpView, d_GameModel);
+	public static void main(String[] p_Args) {
+		GameModelNew l_GameModel = new GameModelNew();
+		CommandPrompt l_CpView =  new CommandPrompt();
+		new GameController(l_CpView, l_GameModel);
 	}
 }
-
