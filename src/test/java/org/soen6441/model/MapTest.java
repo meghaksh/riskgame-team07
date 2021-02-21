d501dadbd83d6831219647dd1bd880b2b0fe3f8a
AkshitaPatel <akshitapatel02@gmail.com>
2021-02-21 13:55:42 +0400

    Modifying method to test AddCountry in Map.java

--- src/test/java/org/soen6441/model/MapTest.java
+++ src/test/java/org/soen6441/model/MapTest.java
@@ -3,6 +3,10 @@ package org.soen6441.model;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -15,15 +19,70 @@ public class MapTest {
 	
 	@Test (expected= Exception.class)
 	public void testAddCountry() throws Exception {
-		
-		String l_country = "india";
-		String l_continent = "asia";
-		
+	
+		Country c1 = new Country("india","asia");
+		Country c2 = new Country("china","asia");
+		Country c3 = new Country("japan","asia");
+		ArrayList<Country> check =  new ArrayList<Country>();
+		check.add(c1);
+		check.add(c2);
+		check.add(c3);
+
 		Map l_map = new Map();
+		l_map.AddCountry("india","asia");
+		l_map.AddCountry("china","asia");
+		l_map.AddCountry("japan","asia");
+
+		
+		for (Continent co: l_map.getContinentList()){
+		if (co.getContinentName().equals("asia")){
+			assertEquals(check,co.getCountryList());
+		}
+		
+	}
+		
+	
+		
+//		l_map.AddCountry(l_country, l_continent);
+//		l_map.AddCountry("china", l_continent);
 		
 		
-		l_map.AddCountry(l_country, l_continent);
-		l_map.AddCountry(l_country, l_continent);
+		
+		// to check if country has been added to the list or not
+//		for(Country c: l_map.getCountryList()){
+//			if(c.getCountryName().contains(l_country)) {
+//				flag=true;
+//			}
+//		}
+		
+		
+		//Boolean flag2=false;
+		//to check if the country has been added to the correct continent or not
+//		for (Continent co: l_map.getContinentList()){
+//			if (co.getContinentName().equals(l_continent)){
+//				for(Country c1: co.getCountryList()){
+//					if(c1.getCountryName().contains(l_country)) {
+//						flag2=true;
+//					}
+//				}
+//			}
+//			
+//		}
+//		
+		
+		
+		//assertTrue(flag);
+		
+
+		
+		//assertTrue(flag2);
+		
+		
+		
+		
+		
+		
+		
 		
 		
 //		try {
