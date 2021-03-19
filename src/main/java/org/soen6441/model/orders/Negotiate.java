581abbafebd714a869cb2f471c65bc0f66a0437a
Meghaksh Brahmbhatt <meghaksh4footprints@gmail.com>
2021-03-19 18:27:35 +0530

    Pushing the Negotiate Order file

---
+++ src/main/java/org/soen6441/model/orders/Negotiate.java
@@ -0,0 +1,13 @@
+package org.soen6441.model.orders;
+
+import org.soen6441.model.Order;
+
+public class Negotiate implements Order {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
