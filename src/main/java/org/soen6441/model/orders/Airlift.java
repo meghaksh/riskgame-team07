b75db0f0c3b5280874b77fb9985cb7364b1301ad
Meghaksh Brahmbhatt <meghaksh4footprints@gmail.com>
2021-03-19 18:26:41 +0530

    Pushing the Airlift Order file

---
+++ src/main/java/org/soen6441/model/orders/Airlift.java
@@ -0,0 +1,13 @@
+package org.soen6441.model.orders;
+
+import org.soen6441.model.Order;
+
+public class Airlift implements Order {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
