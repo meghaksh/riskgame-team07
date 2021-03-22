7d74513592a5a27855b590f3b4ec8d33ad503f21
Meghaksh Brahmbhatt <meghaksh4footprints@gmail.com>
2021-03-22 22:56:58 +0530

    some of the test cases to be implemented in the orders

---
+++ src/test/java/org/soen6441/model/orders/AdvanceTest.java
@@ -0,0 +1,19 @@
+package org.soen6441.model.orders;
+
+public class AdvanceTest {
+/*
+ * 1) When Advance(transfer/attack) on the non adjacent territory
+ * 2) If Transfer is working properly. Source and target countries belong to same player and are adjacent
+ * 3) If Attack is working properly. Source and target countries belong to different players and are adjacent
+ * 4) At the end of the turn, card is assigned properly to all the players who conquer at least one country in the turn
+ * 5) Player's non deployed armies in this turn is adjusted in the next turn
+ * 6) Bomb command halves the number of armies on the target territory
+ * 7) Bomb command on the non adjacent terittory 
+ * 8) Any player that doesn't own a single country is removed from the player's list at the end of the turn (important to win the game)
+ * 9) scenario when defender has more armies than attacker in the attack 
+ * 10) blockade and negotiate orders working properly 
+ * 11) deploy on another player's territory 
+ * 12) any other command or invalid string while issuing the order 
+ * 13) if player a has negotiated with b, then attack from both the player can not happen on each other. 
+ * */
+}
