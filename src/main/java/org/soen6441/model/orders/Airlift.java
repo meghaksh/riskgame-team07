588b05b1554b31f2d92b75bb1d6e71e39a1ff67b
AkshitaPatel <akshitapatel02@gmail.com>
2021-03-23 17:32:33 +0530

    Checking coding conventions in Airlift and Blockade

--- src/main/java/org/soen6441/model/orders/Airlift.java
+++ src/main/java/org/soen6441/model/orders/Airlift.java
@@ -1,15 +1,5 @@
 package org.soen6441.model.orders;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.Map.Entry;
-
 import org.soen6441.model.Country;
 import org.soen6441.model.Order;
 import org.soen6441.model.Player;
@@ -19,58 +9,40 @@ public class Airlift implements Order {
 	Country d_SourceCountry,d_TargetCountry;
 	int d_NumArmies;
 
-
-	
-	public Airlift(Player l_player, Country l_SourceCountry, Country l_TargetCountry, int l_NumArmies)
-	{
-		d_Player = l_player;
-		d_SourceCountry = l_SourceCountry;
-		d_TargetCountry = l_TargetCountry;
-		d_NumArmies = l_NumArmies;
-		
+	public Airlift(Player p_player, Country p_SourceCountry, Country p_TargetCountry, int p_NumArmies) {
+		d_Player = p_player;
+		d_SourceCountry = p_SourceCountry;
+		d_TargetCountry = p_TargetCountry;
+		d_NumArmies = p_NumArmies;
 	}
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		 
-		 if(isValid())
-		 {
-			 d_SourceCountry.setNoOfArmies(d_SourceCountry.getNoOfArmies()-d_NumArmies);
-			 d_TargetCountry.setNoOfArmies(d_TargetCountry.getNoOfArmies()+d_NumArmies);
-		 }
-		 d_Player.removeCard("Airlift");
-		
-		
+		if(isValid()) {
+			d_SourceCountry.setNoOfArmies(d_SourceCountry.getNoOfArmies()-d_NumArmies);
+			d_TargetCountry.setNoOfArmies(d_TargetCountry.getNoOfArmies()+d_NumArmies);
+		}
+		d_Player.removeCard("Airlift");
 	}
-	
-	
-	public boolean isValid()
-	{
-		if(d_SourceCountry==d_TargetCountry)
-		{
+
+	public boolean isValid() {
+		if(d_SourceCountry==d_TargetCountry) {
 			d_Player.setResult("The source country and target country cannot be same!");
 			return false;
 		}
-		else if(d_SourceCountry.getNoOfArmies()-d_NumArmies < 1)
-		{
+		else if(d_SourceCountry.getNoOfArmies()-d_NumArmies < 1) {
 			d_Player.setResult("The source country should be left with atleast one army!");
 			return false;
 		}
-		else
-		{
-			if(d_Player.getCountryList().contains(d_SourceCountry) && d_Player.getCountryList().contains(d_TargetCountry))
-			{
+		else {
+			if(d_Player.getCountryList().contains(d_SourceCountry) && d_Player.getCountryList().contains(d_TargetCountry)) {
 				d_Player.setResult("The source country and target country belong to the same player");
 				return true;
 			}
-			else
-			{
+			else {
 				d_Player.setResult("You can only airlift armies to your own countries. "+d_TargetCountry+"does not belongs to "+d_Player);
 				return false;
 			}
 		}
-		
 	}
-
 }
--- src/main/java/org/soen6441/model/orders/Blockade.java
+++ src/main/java/org/soen6441/model/orders/Blockade.java
@@ -9,65 +9,55 @@ import org.soen6441.model.Order;
 import org.soen6441.model.Player;
 
 public class Blockade implements Order {
-	
+
 	private Player d_Player;
 	private Country d_Country;
 	private GameModelNew d_gameobj;
-	
+
 	public Blockade(Player p_player, Country p_TempCountry) {
 		d_Player = p_player;
 		d_Country = p_TempCountry;
 		this.d_gameobj = d_Player.getGameModel();
-	
 	}
-
 
 	@Override
 	public void execute() {
-		if(isValid())
-		{
+		if(isValid()) {
 			d_Country.setNoOfArmies(d_Country.getNoOfArmies()*3);
-			
 			d_Player.getCountryList().remove(d_Country);
 			for(Player l_Player : d_gameobj.getAllPlayers()) {
-				if(l_Player.getPlayerName()=="Neutral Player")
-				{
-//					System.out.println("play"+l_Player);
+				if(l_Player.getPlayerName()=="Neutral Player") {
+					//System.out.println("play"+l_Player);
 					d_Country.setCountryOwnerPlayer(l_Player);
 					l_Player.setPlayerArmies(d_Country.getNoOfArmies());
 					l_Player.addCountry(d_Country);
-//					System.out.println("inside blockade");
+					//System.out.println("inside blockade");
 				}
 			}
-
 		}
 		d_Player.removeCard("Blockade");
-//		System.out.println("outside blockade");
+		//System.out.println("outside blockade");
 	}
-	
-	public boolean isValid()
-	{
+
+	public boolean isValid() {
 		int l_Flag=0;
-		
 		if(!d_Player.getCardList().contains("Blockade")) {
 			System.out.println("Player does not have a blockade card");
 			return false;
 		}
-
-			Iterator<Country>l_It = d_Player.getCountryList().iterator();
-			while(l_It.hasNext()) {
-				Country l_TempCountry = (Country)l_It.next() ;
-				if(d_Country==l_TempCountry) {
-					l_Flag=1;
-					break;
-				}
+		Iterator<Country>l_It = d_Player.getCountryList().iterator();
+		while(l_It.hasNext()) {
+			Country l_TempCountry = (Country)l_It.next() ;
+			if(d_Country==l_TempCountry) {
+				l_Flag=1;
+				break;
 			}
-			if(l_Flag==1) {
-				return true;
-			} else {
-				d_Player.setResult("\nThis country "+d_Country.getCountryName()+" doesnot belongs to "+d_Player.getPlayerName());
-				return false;
-			}	
+		}
+		if(l_Flag==1) {
+			return true;
+		} else {
+			d_Player.setResult("\nThis country "+d_Country.getCountryName()+" doesnot belongs to "+d_Player.getPlayerName());
+			return false;
+		}	
 	}
-
 }
