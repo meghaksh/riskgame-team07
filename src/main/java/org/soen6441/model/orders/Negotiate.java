eb8c3888b2c47567231e02ac9f95e31d8e8c8305
AkshitaPatel <akshitapatel02@gmail.com>
2021-03-23 23:03:26 +0530

    Adding getter setter method for player in negotiate order, creating test cases for negotiate order.

--- src/main/java/org/soen6441/model/orders/Negotiate.java
+++ src/main/java/org/soen6441/model/orders/Negotiate.java
@@ -4,11 +4,11 @@ import org.soen6441.model.Order;
 import org.soen6441.model.Player;
 
 public class Negotiate implements Order {
-	Player d_SourcePlayer,d_TargetPlayer;
+	private Player d_SourcePlayer,d_TargetPlayer;
 
 	public Negotiate(Player p_SourcePlayer, Player p_TargetPlayer) {
 		System.out.println("Negotiate Created : " + p_SourcePlayer.getPlayerName() + " : " + p_TargetPlayer.getPlayerName());
-		d_SourcePlayer = p_SourcePlayer;
+		set_SourcePlayer(p_SourcePlayer);
 		d_TargetPlayer = p_TargetPlayer;
 
 	}
@@ -16,16 +16,25 @@ public class Negotiate implements Order {
 	@Override
 	public void execute() {
 		/*If Player has negotiate card, then it will add target player to negotiatedPlayerList and then remove that card*/
-		if(this.d_SourcePlayer.getCardList().contains("Negotiate")) {
-			this.d_SourcePlayer.getNegotiatedPlayerList().add(d_TargetPlayer);
-			this.d_TargetPlayer.getNegotiatedPlayerList().add(d_SourcePlayer);
-			d_SourcePlayer.removeCard("Negotiate");
+		if(this.get_SourcePlayer().getCardList().contains("Negotiate")) {
+			this.get_SourcePlayer().getNegotiatedPlayerList().add(d_TargetPlayer);
+			this.d_TargetPlayer.getNegotiatedPlayerList().add(get_SourcePlayer());
+			get_SourcePlayer().removeCard("Negotiate");
+			d_SourcePlayer.setResult("Negotation with "+d_TargetPlayer.getPlayerName()+" successfull.");
 		}
 		else
 		{
-			d_SourcePlayer.setResult("\n"+d_SourcePlayer.getPlayerName()+" does not own Negotiate Card for Diplomacy with "+d_TargetPlayer.getPlayerName());
+			get_SourcePlayer().setResult("\n"+get_SourcePlayer().getPlayerName()+" does not own Negotiate Card for Diplomacy with "+d_TargetPlayer.getPlayerName());
 		}
 
+	}
+
+	public Player get_SourcePlayer() {
+		return d_SourcePlayer;
+	}
+
+	public void set_SourcePlayer(Player d_SourcePlayer) {
+		this.d_SourcePlayer = d_SourcePlayer;
 	}
 
 
