8db486c34c4b9eb642574fb885e635d14070e585
ZealPatel15 <zhy15798@gmail.com>
2021-03-24 23:35:43 +0530

    Removing OrderTest file of Build1

--- src/test/java/org/soen6441/model/JUnitTestSuite.java
+++ src/test/java/org/soen6441/model/JUnitTestSuite.java
@@ -8,7 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
  * JUnit Test Suite Class to run all the Test cases
  */
 @RunWith(Suite.class)
-@SuiteClasses({ GameModelTest.class, MapTest.class, OrderTest.class })
+@SuiteClasses({ GameModelTest.class, MapTest.class})
 public class JUnitTestSuite {
 
 }
