e65f56945229f67eed917ded9590bf89c28d7bb1
Sarvesh <raytersarvesh8@gmail.com>
2021-04-04 12:52:38 +0530

    implemented code to addborders in the loadconquestmap of adaptee class

--- src/main/java/org/soen6441/adapterpattern/Adaptee.java
+++ src/main/java/org/soen6441/adapterpattern/Adaptee.java
@@ -4,14 +4,12 @@ import java.io.File;
 import java.util.Scanner;
 
 import org.soen6441.controller.GameEngine;
-import org.soen6441.model.Continent;
 
 public class Adaptee {
 
 public String loadConquestMap(String p_S,GameEngine p_GameEngine) {
 	try {
-		System.out.println("reached in loadmap");
-		String l_Path="resource\\",l_Result;
+		String l_Path="resource\\";
 		File l_File =new File(l_Path+p_S);
 		Scanner l_Sc = new Scanner(l_File);
 		while(l_Sc.hasNextLine())
@@ -23,8 +21,6 @@ public String loadConquestMap(String p_S,GameEngine p_GameEngine) {
 					while(!l_Line.equals("") && l_Sc.hasNextLine())
 					{
 						String[] l_Arr = l_Line.split("=", 2);
-						System.out.println(l_Arr[0]+" This is name");
-						System.out.println(l_Arr[1]+" This is control value");
 						p_GameEngine.getGameModel().getMap().addContinent(l_Arr[0],l_Arr[1]);
 						l_Line=l_Sc.nextLine();
 					}
@@ -38,11 +34,35 @@ public String loadConquestMap(String p_S,GameEngine p_GameEngine) {
 						String[] l_Arr1=l_Line.split(",");
 						p_GameEngine.getGameModel().getMap().addCountry(l_Arr1[0], l_Arr1[3]);
 						l_Line=l_Sc.nextLine();
-					}
+						
 				}
 			}
-		}catch(Exception p_E)
-		{System.out.println(p_E);}
+		}
+		File l_File2 =new File(l_Path+p_S);
+		Scanner l_Sc2 = new Scanner(l_File2);
+		while(l_Sc2.hasNextLine())
+		{
+			String l_Line=l_Sc2.nextLine();
+			if(l_Line.contains("Territories"))
+			{
+				l_Line=l_Sc2.nextLine();
+				while(!l_Line.equals("") && l_Sc2.hasNextLine())
+				{
+					String[] l_Arr1=l_Line.split(",");
+					for(int k=4;k<l_Arr1.length;k++)
+					{
+						
+						p_GameEngine.getGameModel().getMap().addBorder(l_Arr1[0], l_Arr1[k]);
+						
+				}
+					l_Line=l_Sc2.nextLine();
+			}
+		}
+		}
+	}catch(Exception p_E)
+		{//System.out.println(p_E);
+		}
+		
 
 		return "success";
 	}
