cea86be55331630509c8eca6e08facac8bf513e9
ZealPatel15 <zhy15798@gmail.com>
2021-04-12 00:02:47 +0530

    Initialising the constructor of CheaterPlayer

--- src/main/java/org/soen6441/strategypattern/CheaterPlayerStrategy.java
+++ src/main/java/org/soen6441/strategypattern/CheaterPlayerStrategy.java
@@ -1,11 +1,21 @@
 package org.soen6441.strategypattern;
 
+import java.util.HashMap;
+import java.util.Random;
+
 import org.soen6441.model.GameModelNew;
 import org.soen6441.model.Order;
 import org.soen6441.model.Player;
 
 public class CheaterPlayerStrategy extends Strategy {
 
+	private Random rand;
+	private GameModelNew d_GameModelNew;
+	private Player d_Player;
+	HashMap <Player,Boolean> d_CheckArmies = new HashMap<>();
+	boolean d_decreasePlayerListSize;
+	
+	
 	/*
 	 * A cheater computer player strategy whose issueOrder() method conquers all the immediate neighboring enemy countries, 
 	 * and then doubles the number of armies on its countries that have enemy neighbors. 
@@ -15,8 +25,12 @@ public class CheaterPlayerStrategy extends Strategy {
 	 * but rather implement the above-stated behavior by directly affecting the map during the order creation phase
 	 * */
 	public CheaterPlayerStrategy(Player p_Player,GameModelNew p_GameModelNew) {
-		
+		this.d_GameModelNew = p_GameModelNew;
+		d_Player = p_Player;
+		rand = new Random();
+		System.out.println("Cheater");
 	}
+	
 	@Override
 	public Order createOrder() {
 		// TODO Auto-generated method stub
