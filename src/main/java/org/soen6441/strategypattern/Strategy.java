3a8f338a42ab46afbc28f40f4dbee5cbce3f2d5a
ZealPatel15 <zhy15798@gmail.com>
2021-04-13 19:24:44 +0530

    Creating a logging object for generating logs for strategy player

--- src/main/java/org/soen6441/strategypattern/Strategy.java
+++ src/main/java/org/soen6441/strategypattern/Strategy.java
@@ -1,7 +1,9 @@
 package org.soen6441.strategypattern;
 
 import org.soen6441.model.Order;
+import org.soen6441.observerpattern.LogEntryBuffer;
 
 public abstract class Strategy {
+	LogEntryBuffer d_Leb = new LogEntryBuffer();
 	public abstract Order createOrder();
 }
