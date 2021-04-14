dc99cc140347b1f4b605de380d1bad0e1e3de87e
rokaliraj <rokalirajkumar@gmail.com>
2021-04-14 23:56:52 +0530

    Adding Strategy testsuite in overall Testsuite

--- src/test/java/JUnitTestSuite.java
+++ src/test/java/JUnitTestSuite.java
@@ -3,13 +3,14 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.soen6441.model.JUnitTestSuiteModel;
 import org.soen6441.model.orders.JUnitTestSuiteOrders;
+import org.soen6441.strategypattern.JUnitTestSuiteStrategy;
 import org.soen6441.utility.state.JunitTestSuiteState;
 
 /**
  * JUnit Test Suite Class to run all the test suits in each test folder. 
  */
 @RunWith(Suite.class)
-@SuiteClasses({JUnitTestSuiteModel.class,JUnitTestSuiteOrders.class,JunitTestSuiteState.class})
+@SuiteClasses({JUnitTestSuiteModel.class,JUnitTestSuiteOrders.class,JunitTestSuiteState.class,JUnitTestSuiteStrategy.class})
 public class JUnitTestSuite {
 
 }
