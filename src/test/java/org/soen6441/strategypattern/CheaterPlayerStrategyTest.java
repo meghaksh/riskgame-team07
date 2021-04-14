9fbbeca4919fa7244371f2e4bc33106b8f816330
AkshitaPatel <akshitapatel02@gmail.com>
2021-04-14 18:16:32 +0530

    Creating test cases package and file for strategy pattern

---
+++ src/test/java/org/soen6441/strategypattern/CheaterPlayerStrategyTest.java
@@ -0,0 +1,14 @@
+package org.soen6441.strategypattern;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CheaterPlayerStrategyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/org/soen6441/strategypattern/RandomPlayerStrategyTest.java
@@ -0,0 +1,14 @@
+package org.soen6441.strategypattern;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RandomPlayerStrategyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
