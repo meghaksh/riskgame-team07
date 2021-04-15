37a696c5902e1c7eeba65824ab5f45f9b1d06224
AkshitaPatel <akshitapatel02@gmail.com>
2021-04-15 13:23:26 +0530

    Setting context for tournament test mode.

--- src/test/java/org/soen6441/controller/GameEngineTest.java
+++ src/test/java/org/soen6441/controller/GameEngineTest.java
@@ -2,9 +2,32 @@ package org.soen6441.controller;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.soen6441.model.GameModelNew;
+import org.soen6441.view.CommandPrompt;
 
 public class GameEngineTest {
+	
+	CommandPrompt d_CpView;
+	GameModelNew d_GameModel;
+	GameEngine d_GameEngine;
+	
+	
+	@Before
+	public void setTestContext() {
+		d_CpView = new CommandPrompt();
+		d_GameModel = new GameModelNew();
+		d_GameEngine = new GameEngine(d_CpView, d_GameModel);
+	}
+	
+	
+	
+	
+	@Test
+	public void testTournamentMode() {
+		
+	}
 
 
 
