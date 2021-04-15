e3738c40885853e97838d5afc9ad407e65028a17
AkshitaPatel <akshitapatel02@gmail.com>
2021-04-15 14:10:49 +0530

    Creating test suite for controller - tournament mode

---
+++ src/test/java/org/soen6441/controller/JUnitTestSuiteController.java
@@ -0,0 +1,11 @@
+package org.soen6441.controller;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ GameEngineTest.class })
+public class JUnitTestSuiteController {
+
+}
