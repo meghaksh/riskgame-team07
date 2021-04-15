ab503297b1d36299f5f5e7eda50ff9ad79fb2057
Meghaksh Brahmbhatt <meghaksh4footprints@gmail.com>
2021-04-15 10:59:52 +0530

    javadoc and coding convention for benevolent and strategy parent class

--- src/main/java/org/soen6441/strategypattern/BenevolentPlayerStrategy.java
+++ src/main/java/org/soen6441/strategypattern/BenevolentPlayerStrategy.java
@@ -16,6 +16,10 @@ import org.soen6441.model.Order;
 import org.soen6441.model.Player;
 import org.soen6441.model.orders.Deploy;
 
+/**
+ * This class implements the Strategy for the benevolent type of player. 
+ * This type of player always deloys on it's weak country and never attacks. 
+ */
 public class BenevolentPlayerStrategy extends Strategy implements Serializable {
 
 	/*
@@ -29,60 +33,77 @@ public class BenevolentPlayerStrategy extends Strategy implements Serializable {
 	private GameModelNew d_GameModelNew;
 	private Player d_Player;
 
+	/**
+	 * Constructor which accepts the Player object and GameModel object to implement strategy. 
+	 * @param p_Player object of the player whose strategy is benevolent. 
+	 * @param p_GameModelNew object of the gamemodel class
+	 */
 	public BenevolentPlayerStrategy(Player p_Player,GameModelNew p_GameModelNew) {
 		this.d_GameModelNew = p_GameModelNew;
 		this.d_Player = p_Player;
 		d_Random = new Random();
 		d_Leb.setResult("Benevolent Player");
 	}
+	
+	/**
+	 * This method returns the weakest country of the player where he wants to deploy armies just to save it. 
+	 * @return returns the country at which armies are deploy
+	 */
 	@Override
 	public Country toDefend() {
-
 		Country l_TempCountry=null;
 		int l_NumberOfArmies = 0;
 		HashMap <Country,Integer> l_PlayerCountryMap = new HashMap<>();
 
-		for(Country l_Country : d_Player.getCountryList())
-		{
+		for(Country l_Country : d_Player.getCountryList()){
 			System.out.println("in for loop "+l_Country+" - "+l_Country.getNoOfArmies());
 			l_PlayerCountryMap.put(l_Country, l_Country.getNoOfArmies());
 			System.out.println(l_Country+" - "+l_Country.getNoOfArmies());
 		}
 		System.out.println("int benevolent treemap is "+l_PlayerCountryMap);
-		List<Entry<Country, Integer>> list = new LinkedList<Entry<Country, Integer>>(l_PlayerCountryMap.entrySet()); 
-		Collections.sort(list, new Comparator<Entry<Country, Integer>>()   
-		{  
-			public int compare(Entry<Country, Integer> o1, Entry<Country, Integer> o2)   
+		List<Entry<Country, Integer>> l_List = new LinkedList<Entry<Country, Integer>>(l_PlayerCountryMap.entrySet()); 
+		Collections.sort(l_List, new Comparator<Entry<Country, Integer>>(){  
+			public int compare(Entry<Country, Integer> l_O1, Entry<Country, Integer> l_O2)   
 			{   
-				return o1.getValue().compareTo(o2.getValue());  
+				return l_O1.getValue().compareTo(l_O2.getValue());  
 			}  
 		});
-		l_TempCountry = list.get(0).getKey();
+		l_TempCountry = l_List.get(0).getKey();
 
 		System.out.println("left for loop  "+l_TempCountry);
 		d_Player.setResult("the Benevolent Player is defefnding country "+l_TempCountry.getCountryName());
 		d_Leb.setResult("the Benevolent Player is defefnding country "+l_TempCountry.getCountryName()+" country with "+l_TempCountry.getNoOfArmies()+" armies");
-		//System.out.println("Weakest Country : " + l_TempCountry.getCountryName() + " Has armies : " + l_TempCountry.getNoOfArmies());
 		return l_TempCountry;
 	}
 
+	/**
+	 * Benevolent player never attacks so it is returning null. 
+	 */
 	@Override
 	public ArrayList<Country> toAttack()
 	{
 		return null;
 	}
 
+	/**
+	 * createOrder method for the benevolent player only returns the deploy order for player's weakest country. 
+	 * @return Order object created
+	 */
 	@Override
 	public Order createOrder() {
 		// TODO Auto-generated method stub
-		Order l_returnOrder=null;
+		Order l_ReturnOrder=null;
 		Country l_DefendCountry1 = toDefend();
 		d_Leb.setResult("in cheater the armies are deployed to -" +l_DefendCountry1);
-		l_returnOrder =  new Deploy(this.d_Player, l_DefendCountry1, Math.max(d_Random.nextInt(d_Player.getPlayerArmies()),2));
-		d_Leb.setResult("in benevolent player the order issued is - "+l_returnOrder);
-		return l_returnOrder;
+		l_ReturnOrder =  new Deploy(this.d_Player, l_DefendCountry1, Math.max(d_Random.nextInt(d_Player.getPlayerArmies()),2));
+		d_Leb.setResult("in benevolent player the order issued is - "+l_ReturnOrder);
+		return l_ReturnOrder;
 	}
 
+	/**
+	 * This method returns the name of the player's strategy
+	 * @return String which indicates the type of player strategy
+	 */
 	@Override
 	public String strategyName() {
 		// TODO Auto-generated method stub
--- src/main/java/org/soen6441/strategypattern/Strategy.java
+++ src/main/java/org/soen6441/strategypattern/Strategy.java
@@ -7,10 +7,34 @@ import org.soen6441.model.Country;
 import org.soen6441.model.Order;
 import org.soen6441.observerpattern.LogEntryBuffer;
 
+/**
+ * This is a parent class for the strategy pattern. 
+ * Each player has their different strategy and they extends this class and implements the methods accordingly. 
+ */
 public abstract class Strategy implements Serializable {
 	LogEntryBuffer d_Leb = new LogEntryBuffer();
+	/**
+	 * This method creates different types of orders for each type of player based on their strategy. 
+	 * @return order created according to the player type 
+	 */
 	public abstract Order createOrder();
+	
+	/**
+	 * This method returns the name of the strategy that player holds. 
+	 * @return strategy name
+	 */
 	public abstract String strategyName();
+	
+	/**
+	 * This method returns the source country for deploy or advance order. 
+	 * @return country to which armies are deployed or from where attack is happening
+	 */
 	public abstract Country toDefend();
+	
+	/**
+	 * This method returns the list of source and target country for the attack. \
+	 * This is internally used for the advance order creation to pass as target source and target country. 
+	 * @return returns list with first parameter as source country and second as target country. 
+	 */
 	public abstract ArrayList<Country> toAttack();
 }
