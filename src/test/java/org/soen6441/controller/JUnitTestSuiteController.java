5ca1a763e8951271f8d15002f2e5d3e03b1ec04a
Sarvesh <raytersarvesh8@gmail.com>
2021-04-15 17:19:29 +0530

    Final Java dOC generation

--- src/test/java/org/soen6441/controller/JUnitTestSuiteController.java
+++ src/test/java/org/soen6441/controller/JUnitTestSuiteController.java
@@ -3,7 +3,9 @@ package org.soen6441.controller;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
+/**
+* JUnit Test Suite Class to run all the Test cases of controller
+*/
 @RunWith(Suite.class)
 @SuiteClasses({ GameEngineTest.class })
 public class JUnitTestSuiteController {
--- src/test/java/org/soen6441/strategypattern/JUnitTestSuiteStrategy.java
+++ src/test/java/org/soen6441/strategypattern/JUnitTestSuiteStrategy.java
@@ -3,7 +3,9 @@ package org.soen6441.strategypattern;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
+/**
+* JUnit Test Suite Class to run all the Test cases of Strategy
+*/
 @RunWith(Suite.class)
 @SuiteClasses({ AggresivePlayerStrategyTest.class, BenevolentPlayerStrategyTest.class, CheaterPlayerStrategyTest.class,
 		RandomPlayerStrategyTest.class })
