5c01adbc5a7e99fc128567436bc415521f56e931
AkshitaPatel <akshitapatel02@gmail.com>
2021-04-15 14:11:20 +0530

    Modifying the main test suite for including test suite for controller

--- src/test/java/JUnitTestSuite.java
+++ src/test/java/JUnitTestSuite.java
@@ -1,6 +1,7 @@
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.soen6441.controller.JUnitTestSuiteController;
 import org.soen6441.model.JUnitTestSuiteModel;
 import org.soen6441.model.orders.JUnitTestSuiteOrders;
 import org.soen6441.strategypattern.JUnitTestSuiteStrategy;
@@ -10,7 +11,7 @@ import org.soen6441.utility.state.JunitTestSuiteState;
  * JUnit Test Suite Class to run all the test suits in each test folder. 
  */
 @RunWith(Suite.class)
-@SuiteClasses({JUnitTestSuiteModel.class,JUnitTestSuiteOrders.class,JunitTestSuiteState.class,JUnitTestSuiteStrategy.class})
+@SuiteClasses({JUnitTestSuiteController.class, JUnitTestSuiteModel.class,JUnitTestSuiteOrders.class,JunitTestSuiteState.class,JUnitTestSuiteStrategy.class})
 public class JUnitTestSuite {
 
 }
